874e7394551b6f58991eb954cb807e1449445003
mattygug <m.schreder@mac.com>
2015-06-23 15:06:13 +0200

    establish ground framework

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,14 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/sources', {
+        templateUrl: 'views/sources.html',
+        controller: 'SourcesCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/sources.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blauesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blauesApp
+ */
+angular.module('blauesApp')
+  .controller('SourcesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

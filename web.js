d651dcc0ce62818b9a75bf23d9581ae12b6bb741
mattygug <m.schreder@mac.com>
2015-06-24 00:37:45 +0200

    added web.js pg

--- web.js
+++ web.js
@@ -6,3 +6,17 @@ var app = express();
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
